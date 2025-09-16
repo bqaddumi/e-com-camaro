00f0e5555fb7ae5283f8012f12a3026e19640438
mohammad shamasmeh <m.shamasneh00@gmial.com>
2025-09-16 16:06:59 +0300

    Add Redux setup files and update App.jsx

---
+++ src/slices/homePageSlice.js
@@ -0,0 +1,12 @@
+/*
+import { createSlice } from '@reduxjs/toolkit'
+import {homePagereducer} from'@reduxjs/toolkit ; 
+/*const  homeSlice = createSlice ({
+    initialState : {itemshome: []},
+    name:' home page', 
+    reducers:{
+        
+    }
+
+})
+    */
