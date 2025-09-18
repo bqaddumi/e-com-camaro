9aaa2c068939915e328836816ba63c67147637d1
khaled kassab <khaledfadikassab@gmail.com>
2025-09-18 16:20:04 +0300

    format code with prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import react from 'react';
-import { Navbar } from './Component/Navbar';
-import { Link } from 'react-router-dom';
-  import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Home } from './Component/Home';
-import { About } from './Component/About';
-import { Contact } from './Component/Contact';
+import react from "react";
+import { Navbar } from "./Component/Navbar";
+import { Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./Component/Home";
+import { About } from "./Component/About";
+import { Contact } from "./Component/Contact";
 
 export const App = () => {
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
-  )
-  }
-  export default App;
+  );
+};
+export default App;
