7517f7999a657649139fddf01cf3d85347baf3a4
mohammad shamasmeh <m.shamasneh00@gmial.com>
2025-09-18 14:25:56 +0300

    chore: alias react-redux Provider to StoreProvider for clarity

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
+import { Provider  as  StoreProvider  } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+  < StoreProvider  store={store}>
 
     <App />
-  </Provider>
+  </ StoreProvider >
 );
