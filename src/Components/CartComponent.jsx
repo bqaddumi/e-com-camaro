5848d3678969c407d4c8b5531f3981b46eb4ee41
mohammad shamasmeh <m.shamasneh00@gmial.com>
2025-09-18 14:42:40 +0300

    fix: apply all PR review changes (class names, StoreProvider alias, CSS cleanup)

--- src/Components/CartComponent.jsx
+++ src/Components/CartComponent.jsx
@@ -1,39 +1,43 @@
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
-import { addItem,removeItem } from "../slices/cartSlice";
-import '../css/CartComponent.css';
+import { addItem, removeItem } from "../slices/cartSlice";
+import "../css/CartComponent.css";
 
-
-function CartComponent (){
-    const items= useSelector ((state) => state.cart.items)  ;
-    const dispatch=useDispatch ();
-const handleAdd=()=>{
-    dispatch( addItem({id :Date.now() , name: "mohammad"}))
-    dispatch ( addItem ({id:Date.now()+1, name :" ali "}))
-};
-    const handleremove=(id)=>{
-       dispatch(removeItem(id))
-    };
-return (
-   
+function CartComponent() {
+  const products = [
+    { id: 1, name: "CAMARO Hat" },
+    { id: 2, name: "CAMARO T-Shirt" },
+    { id: 3, name: "CAMARO Mug" },
+  ];
+  const items = useSelector((state) => state.cart.items);
+  const dispatch = useDispatch();
+  const handleAdd = (id, name) => {
+    dispatch(addItem({ id, name }));
+  };
+  const handleRemove = (id) => {
+    dispatch(removeItem(id));
+  };
+  return (
     <div className="cart-container">
-      <button onClick={handleAdd}>add Item</button>
+      <div className=" product-action">
+        {products.map((p) => (
+          <button className="cart-add-btn"key={p.id} onClick={() => handleAdd(p.id, p.name)}>
+            {" "}
+            Add item{" "}
+          </button>
+        ))}
+      </div>
 
-        
-
-         {items.length===0? "cart is empty ":"there are items in the cart" }
-         <ul>
-            {items.map((item)=>(
-            <li key={item.id}>
-                {item.name}
-                <button onClick={() =>handleremove(item.id)}>remove Item</button>
-            </li>
-        ))} 
-       </ul>
-
-
+      {items.length === 0 ? "cart is empty " : `there are ${items.length} items in the cart`}
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            {item.name}
+            <button className="cart-remove-btn" onClick={() => handleRemove(item.id)}>remove Item</button>
+          </li>
+        ))}
+      </ul>
     </div>
-)
+  );
 }
- export default CartComponent; 
-
+export default CartComponent;
--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,11 @@ import { configureStore}from '@reduxjs/toolkit'
 import cartReducer from '../slices/cartSlice'
 export const store =configureStore ({
     reducer: {
-        //myFavorites:myFavoritesreducer,
-       // user:userReducer,
-        //homePage:homePagereducer,
+ 
+      
+       
         cart:cartReducer,
-       // theme:themereducer
+      
 
 
 
